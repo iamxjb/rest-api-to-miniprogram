db21cea1561037a113ec2311df74d2f718ae1457
jianbo <20397795@qq.com>
2019-04-02 22:06:23 +0800

    增加filterEmoji方法

--- includes/ram-util.php
+++ includes/ram-util.php
@@ -440,29 +440,41 @@ function decrypt_data( $appid, $sessionKey, $encryptedData, $iv, &$data ) {
     return $errors['OK'];
 }
 
-  function get_client_ip()
-    {
-        foreach (array(
-                    'HTTP_CLIENT_IP',
-                    'HTTP_X_FORWARDED_FOR',
-                    'HTTP_X_FORWARDED',
-                    'HTTP_X_CLUSTER_CLIENT_IP',
-                    'HTTP_FORWARDED_FOR',
-                    'HTTP_FORWARDED',
-                    'REMOTE_ADDR') as $key) {
-            if (array_key_exists($key, $_SERVER)) {
-                foreach (explode(',', $_SERVER[$key]) as $ip) {
-                    $ip = trim($ip);
-                    //会过滤掉保留地址和私有地址段的IP，例如 127.0.0.1会被过滤
-                    //也可以修改成正则验证IP
-                    if ((bool) filter_var($ip, FILTER_VALIDATE_IP,
-                                    FILTER_FLAG_IPV4 |
-                                    FILTER_FLAG_NO_PRIV_RANGE |
-                                    FILTER_FLAG_NO_RES_RANGE)) {
-                        return $ip;
-                    }
+function get_client_ip()
+{
+    foreach (array(
+                'HTTP_CLIENT_IP',
+                'HTTP_X_FORWARDED_FOR',
+                'HTTP_X_FORWARDED',
+                'HTTP_X_CLUSTER_CLIENT_IP',
+                'HTTP_FORWARDED_FOR',
+                'HTTP_FORWARDED',
+                'REMOTE_ADDR') as $key) {
+        if (array_key_exists($key, $_SERVER)) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+                $ip = trim($ip);
+                //会过滤掉保留地址和私有地址段的IP，例如 127.0.0.1会被过滤
+                //也可以修改成正则验证IP
+                if ((bool) filter_var($ip, FILTER_VALIDATE_IP,
+                                FILTER_FLAG_IPV4 |
+                                FILTER_FLAG_NO_PRIV_RANGE |
+                                FILTER_FLAG_NO_RES_RANGE)) {
+                    return $ip;
                 }
             }
         }
-        return null;
     }
+    return null;
+}
+
+function filterEmoji($str)
+{
+  $str = preg_replace_callback(
+    '/./u',
+    function (array $match) {
+      return strlen($match[0]) >= 4 ? '' : $match[0];
+    },
+    $str);
+
+  return $str;
+}
