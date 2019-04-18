3d77a3bd3ca66c0b6dcc61ac9502b7c2c39ac167
jianbo <20397795@qq.com>
2019-04-18 09:57:08 +0800

    去掉解析腾讯视频的代码，解决加载过多腾讯视频超时错误。

--- includes/filter/ram-custom-content.php
+++ includes/filter/ram-custom-content.php
@@ -2,40 +2,18 @@
 //解析腾讯视频，只支持一个腾讯视频
 function custocm_content_filter($content) {
 
-    
-    // $siteurl = get_option('siteurl');
- //     $upload_dir = wp_upload_dir();
- //     $content = str_replace( 'http:'.strstr($siteurl, '//'), 'https:'.strstr($siteurl, '//'), $content);
- //     $content = str_replace( 'http:'.strstr($upload_dir['baseurl'], '//'), 'https:'.strstr($upload_dir['baseurl'], '//'), $content);
-    preg_match('/https\:\/\/v.qq.com\/x\/(\S*)\/(\S*)\.html/',$content,$matches);
-    if($matches)
+    $_content=$content;
+    if(is_single() || is_home()|| is_feed())
     {
-        $vids=$matches[2];
-
-        //$url='http://vv.video.qq.com/getinfo?vid='.$vids.'&defaultfmt=auto&otype=json&platform=1&defn=fhd&charge=0';
-        //  defaultfmt： 1080P-fhd，超清-shd，高清-hd，标清-sd
-        $url='http://vv.video.qq.com/getinfo?vid='.$vids.'&defaultfmt=auto&otype=json&platform=11001&defn=fhd&charge=0';
-        $res = file_get_contents($url);
-        if($res)
+        $vcontent =get_post_qq_video($content);
+        if(!empty($vcontent))
         {
-            $str = substr($res,13,-1);
-            $newStr =json_decode($str,true);        
-            //$videoUrl= $newStr['vl']['vi'][0]['ul']['ui'][2]['url'].$newStr['vl']['vi'][0]['fn'].'?vkey='.$newStr['vl']['vi'][0]['fvkey']; 
-            $videoUrl= $newStr['vl']['vi'][0]['ul']['ui'][0]['url'].$newStr['vl']['vi'][0]['fn'].'?vkey='.$newStr['vl']['vi'][0]['fvkey']; 
-            $contents = preg_replace('~<video (.*?)></video>~s','<video src="'.$videoUrl.'" controls="controls" width="100%"></video>',$content);
-            return $contents;
+            $_content=$vcontent;
+        }
+    }
 
-        }
-        else
-        {
-            return $content;
-        }
-        
-    }
-    else
-    {
-        return $content;
-    }
+    return $_content;
+
 }
 
 
