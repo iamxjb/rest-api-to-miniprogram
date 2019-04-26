ff8ed051315ce2f126938cfb4d2bdf4217b1d6f3
jianbo <20397795@qq.com>
2019-04-26 15:18:27 +0800

    调整插件tab页jquery加载方式

--- includes/settings/wp-wechat-config.php
+++ includes/settings/wp-wechat-config.php
@@ -12,12 +12,11 @@ function weixinapp_create_menu() {
 }
 
 function get_jquery_source() {
-        $url = plugins_url('',__FILE__);
-        echo '<script type="text/javascript" src="'.plugins_url().'/rest-api-to-miniprogram/includes/js/tab/jquery.min.js?ver=1.10.1"></script>';
+        $url = plugins_url('',__FILE__);        
+        wp_enqueue_script("jquery");
         wp_enqueue_style("tabs", plugins_url()."/rest-api-to-miniprogram/includes/js/tab/tabs.css", false, "1.0", "all");
-
         wp_enqueue_script("tabs", plugins_url()."/rest-api-to-miniprogram/includes/js/tab/tabs.min.js", false, "1.0");
-        }
+}
 
 function register_weixinappsettings() {
     // 注册设置
