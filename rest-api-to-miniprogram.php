528bb2392c4db04c7c6888ca47c66c503cc09f1e
jianbo <20397795@qq.com>
2021-02-23 16:26:40 +0800

    调整版本

--- rest-api-to-miniprogram.php
+++ rest-api-to-miniprogram.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: REST API TO MiniProgram
+Plugin Name: REST API TO MiniProgram 微慕小程序
 Plugin URI: https://www.minapper.com
 Description: 为微信小程序、app提供定制化WordPress REST API json 输出.
-Version: 4.0.2
+Version: 4.0.3
 Author: jianbo
 Author URI: https://www.minapper.com
 License: GPL v3
